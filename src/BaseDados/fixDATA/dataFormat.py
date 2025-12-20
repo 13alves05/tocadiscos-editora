168a6a955e861d7a3fc09e0120b5f2ca7989f780
bmgoliveira-UA <bmgoliveira@ua.pt>
2025-12-20 14:50:34 +0000

    BaseDados/FixDATA - commented files
    textCommit

--- src/BaseDados/fixDATA/dataFormat.py
+++ src/BaseDados/fixDATA/dataFormat.py
@@ -1,44 +1,44 @@
-from random import randint
+# from random import randint
 
-def feedAuthors (authorsDict, line):
-    authorsDict[int(line['artist_id'])] = {
-                    'artist_name' : line['artist_name'],
-                    'artist_nacionality' :line['artist_nacionality'],
-                    'album_title': [(int(line['album_id']), line['album_title'])],
-                    'rights_percentage' : randint(10, 50),
-                    'total_earned' : round(float(line['track_price']) * int(line['track_interest']),2)
-                }
-
-
-def updateAuthors(authorsDict, line):
-    authorsDict[int(line['artist_id'])]['album_title'].append((int(line['album_id']), line['album_title']))
-    authorsDict[int(line['artist_id'])]['total_earned'] += round(float(line['track_price']) * int(line['track_interest']),2)
+# def feedAuthors (authorsDict, line):
+#     authorsDict[int(line['artist_id'])] = {
+#                     'artist_name' : line['artist_name'],
+#                     'artist_nacionality' :line['artist_nacionality'],
+#                     'album_title': [(int(line['album_id']), line['album_title'])],
+#                     'rights_percentage' : randint(10, 50),
+#                     'total_earned' : round(float(line['track_price']) * int(line['track_interest']),2)
+#                 }
 
 
-
-def feedAlbums (albumsDict, line):
-    albumsDict[int(line['album_id'])] = {
-                    'album_title': line['album_title'],
-                    'artist_name': line['artist_name'],
-                    'album_genere': line['track_genres'],
-                    'album_date': line['track_date_recorded'],
-                    'unites_sold': int(line['track_interest']),
-                    'album_price': float(line['track_price']),
-                    'tracks': [(int(line['track_id']),line['track_title'])],
-                }
-
-
-def updateAlbums(albumsDict, line):
-
-    #update unidades vendidas
-    albumsDict[int(line['album_id'])]['unites_sold'] += int(line['track_interest'])
-
-    #update preço album
-    albumsDict[int(line['album_id'])]['album_price'] += float(line['track_price'])
-
-    #update tracks
-    albumsDict[int(line['album_id'])]['tracks'].append((int(line['track_id']),line['track_title'])) 
+# def updateAuthors(authorsDict, line):
+#     authorsDict[int(line['artist_id'])]['album_title'].append((int(line['album_id']), line['album_title']))
+#     authorsDict[int(line['artist_id'])]['total_earned'] += round(float(line['track_price']) * int(line['track_interest']),2)
 
 
 
-# def feedAuthentication():
+# def feedAlbums (albumsDict, line):
+#     albumsDict[int(line['album_id'])] = {
+#                     'album_title': line['album_title'],
+#                     'artist_name': line['artist_name'],
+#                     'album_genere': line['track_genres'],
+#                     'album_date': line['track_date_recorded'],
+#                     'unites_sold': int(line['track_interest']),
+#                     'album_price': float(line['track_price']),
+#                     'tracks': [(int(line['track_id']),line['track_title'])],
+#                 }
+
+
+# def updateAlbums(albumsDict, line):
+
+#     #update unidades vendidas
+#     albumsDict[int(line['album_id'])]['unites_sold'] += int(line['track_interest'])
+
+#     #update preço album
+#     albumsDict[int(line['album_id'])]['album_price'] += float(line['track_price'])
+
+#     #update tracks
+#     albumsDict[int(line['album_id'])]['tracks'].append((int(line['track_id']),line['track_title'])) 
+
+
+
+# # def feedAuthentication():
--- src/BaseDados/fixDATA/organizeData.py
+++ src/BaseDados/fixDATA/organizeData.py
@@ -1,116 +1,116 @@
-from csv import DictReader
-from dataFormat import feedAlbums, updateAlbums, feedAuthors, updateAuthors
+# from csv import DictReader
+# from dataFormat import feedAlbums, updateAlbums, feedAuthors, updateAuthors
 
-#ORGANIZING DATA IN DICTIONARIES
-authorsDict = {}
-albumsDict = {}
+# #ORGANIZING DATA IN DICTIONARIES
+# authorsDict = {}
+# albumsDict = {}
 
-# transforma cada line numa lista
-tracksUsableFile = open('data/raw_tracks.csv', 'r', encoding='utf-8-sig' )
-tracksUsableData = list(DictReader(tracksUsableFile.readlines())) 
-tracksUsableFile.close()
-
-
-## organize data into authors and albums tables
-
-for line in tracksUsableData[1:]:
-
-    # descartar entradas desnecessárias
-    try:
-        artist_id = int(line['artist_id'])
-        album_id = int(line['album_id'])
-    except:
-        continue
+# # transforma cada line numa lista
+# tracksUsableFile = open('data/raw_tracks.csv', 'r', encoding='utf-8-sig' )
+# tracksUsableData = list(DictReader(tracksUsableFile.readlines())) 
+# tracksUsableFile.close()
 
 
-    # Inicializar o dicionário
-    if len(authorsDict) == 0 and len(albumsDict) == 0:
-        feedAuthors(authorsDict, line)
-        feedAlbums(albumsDict, line) 
+# ## organize data into authors and albums tables
 
-    # Adicionar novas entradas OU atualizar entradas existentes
-    if len(authorsDict) > 0 and len(albumsDict) > 0:
+# for line in tracksUsableData[1:]:
 
-        # Authors
-        # Adicionar novo Autor
-        if  artist_id not in authorsDict:
-            try:
-                feedAuthors(authorsDict, line)
-            except:
-                continue
-
-        # Adicionar dados a autor já existente
-        elif artist_id in authorsDict:
-            try:
-                updateAuthors(authorsDict, line)
-            except:
-                continue
+#     # descartar entradas desnecessárias
+#     try:
+#         artist_id = int(line['artist_id'])
+#         album_id = int(line['album_id'])
+#     except:
+#         continue
 
 
-        # Albums
-        # Adicionar novo album
-        if album_id not in albumsDict:
-            try:
-                feedAlbums(albumsDict, line)
-            except:
-                continue
+#     # Inicializar o dicionário
+#     if len(authorsDict) == 0 and len(albumsDict) == 0:
+#         feedAuthors(authorsDict, line)
+#         feedAlbums(albumsDict, line) 
+
+#     # Adicionar novas entradas OU atualizar entradas existentes
+#     if len(authorsDict) > 0 and len(albumsDict) > 0:
+
+#         # Authors
+#         # Adicionar novo Autor
+#         if  artist_id not in authorsDict:
+#             try:
+#                 feedAuthors(authorsDict, line)
+#             except:
+#                 continue
+
+#         # Adicionar dados a autor já existente
+#         elif artist_id in authorsDict:
+#             try:
+#                 updateAuthors(authorsDict, line)
+#             except:
+#                 continue
 
 
-        # Adicionar dados a a album já existente
-        elif album_id in albumsDict:
-            try:
-                updateAlbums(albumsDict, line)
-            except:
-                continue
-
-# ----FIM----  ORGANIZING DATA IN DICTIONARIES
-
+#         # Albums
+#         # Adicionar novo album
+#         if album_id not in albumsDict:
+#             try:
+#                 feedAlbums(albumsDict, line)
+#             except:
+#                 continue
 
 
-# MAKING DIFFERENT TABLES
-authorsData = []
-albumsData = []
+#         # Adicionar dados a a album já existente
+#         elif album_id in albumsDict:
+#             try:
+#                 updateAlbums(albumsDict, line)
+#             except:
+#                 continue
 
-authorsColumns = list(authorsDict[1].keys())
-authorsColumns.insert(0, 'author_id')
-albumsColumns = list(albumsDict[1].keys())
-albumsColumns.insert(0, 'album_id')
+# # ----FIM----  ORGANIZING DATA IN DICTIONARIES
 
-def dict_2_array (dicionario, lista):
-    for index in dicionario:
-        entry = list(dicionario[index].values())
 
-        for val in range(0,len(entry)):
-            entry[val] = str(entry[val])
-        entry.insert(0, str(index))
-        entry = ','.join(entry) + '\n'
 
-        lista.append(entry)
+# # MAKING DIFFERENT TABLES
+# authorsData = []
+# albumsData = []
+
+# authorsColumns = list(authorsDict[1].keys())
+# authorsColumns.insert(0, 'author_id')
+# albumsColumns = list(albumsDict[1].keys())
+# albumsColumns.insert(0, 'album_id')
+
+# def dict_2_array (dicionario, lista):
+#     for index in dicionario:
+#         entry = list(dicionario[index].values())
+
+#         for val in range(0,len(entry)):
+#             entry[val] = str(entry[val])
+#         entry.insert(0, str(index))
+#         entry = ','.join(entry) + '\n'
+
+#         lista.append(entry)
         
 
-# dict_2_array(authorsDict)
-dict_2_array(authorsDict, authorsData)
-dict_2_array(albumsDict, albumsData)
+# # dict_2_array(authorsDict)
+# dict_2_array(authorsDict, authorsData)
+# dict_2_array(albumsDict, albumsData)
 
 
-#adding columns to the begining of the array 
+# #adding columns to the begining of the array 
 
-authorsData.insert(0, ','.join(authorsColumns) + '\n')
-albumsData.insert(0, ','.join(albumsColumns) + '\n')
-
-
-## creating authors table
-
-authorsTable = open('data/authors_table.csv', 'w', encoding='utf-8-sig')
-authorsTable.writelines(authorsData)
-authorsTable.close()
+# authorsData.insert(0, ','.join(authorsColumns) + '\n')
+# albumsData.insert(0, ','.join(albumsColumns) + '\n')
 
 
-## creating albums table
+# ## creating authors table
 
-albumsTable = open('data/albums_table.csv', 'w', encoding='utf-8-sig')
-albumsTable.writelines(albumsData)
-albumsTable.close()
+# authorsTable = open('data/authors_table.csv', 'w', encoding='utf-8-sig')
+# authorsTable.writelines(authorsData)
+# authorsTable.close()
 
 
-# ----FIM----  MAKING DIFFERENT TABLES
+# ## creating albums table
+
+# albumsTable = open('data/albums_table.csv', 'w', encoding='utf-8-sig')
+# albumsTable.writelines(albumsData)
+# albumsTable.close()
+
+
+# # ----FIM----  MAKING DIFFERENT TABLES
